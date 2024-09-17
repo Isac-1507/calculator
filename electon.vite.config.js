4383521a6abff0f860fbf3579887e351dd658a47
Isac-1507 <53452072+Isac-1507@users.noreply.github.com>
2024-09-17 20:54:29 +0100

    Prettify

--- electon.vite.config.js
+++ electon.vite.config.js
@@ -1,12 +1,12 @@
 // electron.vite.config.js
 export default {
-    main: {
-      // vite config options
-    },
-    preload: {
-      // vite config options
-    },
-    renderer: {
-      // vite config options
-    }
+  main: {
+    // vite config options
+  },
+  preload: {
+    // vite config options
+  },
+  renderer: {
+    // vite config options
   }
+}
